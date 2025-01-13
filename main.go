eaf5ea56eae767b6c6ab12a1f34d00cf0fd0e257
Justin Kirk <justin.c.kirk@gmail.com>
2025-01-12 23:18:06 -0500

    starts json

--- main.go
+++ main.go
@@ -2,40 +2,21 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"log"
 	"os"
 	"regexp"
 	"strings"
 )
 
-/*
-LABELS
-after address message area1
-summary
-deposits and additions
-checks paid section3
-atm debit withdrawal
-atm debit withdrawal
-electronic withdrawal
-post fees message
-post fees message
-dre portrait disclosure message area
-*/
-
-/*
-regex patterns
-
-amount at end of line
-[0-9]{1,3},?[0-9]{1,3},?[0-9]{1,3}.[0-9]{2})$
-from chatgpt
-(?<=\b|\s)\$(\d{1,3}(?:,\d{3})*(?:\.\d{2})?)|\d{1,3}(?:,\d{3})*(?:\.\d{2})?$
-
-date at start of line
-^([0-9]{2}\/[0-9]{2})
-*/
-
 var pdfTxtPath = "data/testpdf.txt"
 var txtPath = "data/test.txt"
+
+// Regexes
+var dateAtStartRegex = regexp.MustCompile(`^([0-9]{2}\/[0-9]{2})`)
+var dateRegex = regexp.MustCompile(`([0-9]{2}\/[0-9]{2})`)
+var amountRegex = regexp.MustCompile(`\$(\d{1,3}(?:,\d{3})*(?:\.\d{2})?)|\d{1,3}(?:,\d{3})*(?:\.\d{2})?$`)
+var checkIdRegex = regexp.MustCompile(`^([0-9]{1,})`)
 
 var labels = []string{
 	"after address message area1",
@@ -48,7 +29,54 @@ var labels = []string{
 	"dre portrait disclosure message area",
 }
 
+type Summary struct {
+	Balance struct {
+		Starting string
+		Ending   string
+	}
+	Deposits    string
+	Checks      string
+	Withdrawals struct {
+		Debit      string
+		Electronic string
+	}
+}
+
+type Deposit struct {
+	Date        string
+	Amount      string
+	Description string
+}
+
+type Withdrawal struct {
+	Date        string
+	Amount      string
+	Description string
+}
+
+type Check struct {
+	ID     int
+	Date   string
+	Amount string
+}
+
+type BankJson struct {
+	CheckingSummary Summary
+	Deposits        []Deposit
+	Withdrawals     struct {
+		Debit      []Withdrawal
+		Electronic []Withdrawal
+	}
+	Checks []Check
+}
+
 func main() {
+
+	jsonFile, err := os.Create("test.json")
+	if err != nil {
+		panic(err)
+	}
+	defer jsonFile.Close()
 
 	file, err := os.Open(pdfTxtPath)
 	if err != nil {
@@ -67,6 +95,7 @@ func main() {
 	startTk := "*start*"
 	endTk := "*end*"
 	label := ""
+	jsonData := BankJson{}
 
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -97,21 +126,43 @@ func main() {
 		if readTxt {
 
 			switch label {
+
 			// summary
 			case labels[1]:
-				_, err = txt.WriteString(line + "\n")
+				var entry []string
+
+				match := amountRegex.FindStringSubmatch(line)
+				if match != nil {
+					entry = append(entry, match[0])
+				}
+
+				sum := amountRegex.ReplaceAllString(line, "")
+				sum = strings.TrimSpace(sum)
+
+				entry = append(entry, sum)
+				row := strings.Join(entry, ",")
+
+				_, err = txt.WriteString(row + "\n")
 				if err != nil {
 					panic(err)
 				}
+
+				switch sum {
+				case "Beginning Balance":
+					jsonData.CheckingSummary.Balance.Starting = match[0]
+				case "Ending Balance":
+					jsonData.CheckingSummary.Balance.Ending = match[0]
+				case "Deposits and Additions":
+					jsonData.CheckingSummary.Deposits = match[0]
+				case "Checks Paid":
+					jsonData.CheckingSummary.Checks = match[0]
+				}
+
 			// deposits
 			case labels[2], labels[4], labels[5]:
-				// case labels[4]:
-				dateRegex := regexp.MustCompile(`^([0-9]{2}\/[0-9]{2})`)
-				amountRegex := regexp.MustCompile(`\$(\d{1,3}(?:,\d{3})*(?:\.\d{2})?)|\d{1,3}(?:,\d{3})*(?:\.\d{2})?$`)
-
 				var entry []string
 
-				match := dateRegex.FindStringSubmatch(line)
+				match := dateAtStartRegex.FindStringSubmatch(line)
 				trimmedLine := dateRegex.ReplaceAllString(line, "")
 				if match != nil {
 					entry = append(entry, match[0])
@@ -133,13 +184,9 @@ func main() {
 				if err != nil {
 					panic(err)
 				}
+
 			// checks
 			case labels[3]:
-
-				checkIdRegex := regexp.MustCompile(`^([0-9]{1,})`)
-				dateRegex := regexp.MustCompile(`([0-9]{2}\/[0-9]{2})`)
-				amountRegex := regexp.MustCompile(`\$(\d{1,3}(?:,\d{3})*(?:\.\d{2})?)|\d{1,3}(?:,\d{3})*(?:\.\d{2})?$`)
-
 				var entry []string
 
 				match := checkIdRegex.FindStringSubmatch(line)
@@ -168,12 +215,6 @@ func main() {
 				if err != nil {
 					panic(err)
 				}
-			// // withdrawals
-			// case labels[4]:
-			// 	_, err = txt.WriteString(line + "\n")
-			// 	if err != nil {
-			// 		panic(err)
-			// 	}
 
 			default:
 				continue
@@ -181,6 +222,10 @@ func main() {
 
 		}
 	}
+
+	encoder := json.NewEncoder(jsonFile)
+	encoder.SetIndent("", "  ")
+	encoder.Encode(jsonData)
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
